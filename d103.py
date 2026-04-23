f841be347a5933e752e20cf7ff3fc6de7c18af68
Yuri Santos <yurikaua979@gmail.com>
2026-04-22 21:40:58 -0300

    update

---
+++ d103.py
@@ -0,0 +1,31 @@
+#jeito que eu fiz
+"""def jogador(nome = None, gols = None):
+    if nome.strip() == '':
+        nome = '<desconhecido>'
+    if gols.isnumeric():
+        gols = int(gols)
+    else:
+        gols = 0
+    print(f'O jogador {nome} fez {gols} gol(s) no campeonato.')
+
+
+n = input('Nome do jogador: ')
+g = input('Número de gols: ')
+
+jogador(n, g)"""
+
+#jeito do professor
+def jogador(nome='<desconhecido>', gol=0):
+    print(f'O jogador {nome} fez {gol} gol(s) no campeonato.')
+
+
+n = input('Nome do jogador: ')
+g = input('Número de gols: ')
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    jogador(gol=g)
+else:
+    jogador(n, g)
